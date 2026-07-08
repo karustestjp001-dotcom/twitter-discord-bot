c43e3cb1486b1dc84928f85dce3c4f23e7b6cccd
karustestjp001-dotcom <asf89608@gmail.com>
2026-07-08 23:51:02 +0800

    fix: switch World is Dancing Bilibili source

--- config_bilibili.py
+++ config_bilibili.py
@@ -3,10 +3,9 @@ FORUM_THREAD_PREFIX = "Bilibili 更新"
 
 WATCH_VIDEOS = [
     "BV11kMh6WEe5",
-    "BV1FxT966Emy",
+    "BV1z5KD64Ejy",
     "BV1dEMb6wE2y",
     "BV15yM86PEna",
-    "BV16xT966EyV",
     "BV1XQTZ6MEcS",
     "BV1EFMF6QE41",
     "BV18yMA6EE9g",
@@ -20,8 +19,7 @@ WATCH_VIDEOS = [
 # Same show can sometimes be uploaded as separate BVIDs. Videos with the same
 # key post updates to the same Discord forum thread.
 THREAD_KEY_OVERRIDES = {
-    "BV1FxT966Emy": "world_is_dancing",
-    "BV16xT966EyV": "world_is_dancing",
+    "BV1z5KD64Ejy": "world_is_dancing",
     "BV1XQTZ6MEcS": "mushoku_tensei",
     "BV1Xi7s6iE3e": "cat_and_dragon",
     "BV16DMb6gEt6": "rick_and_morty_s9",
